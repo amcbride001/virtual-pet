6bb4ffeff63ba6a3a7fa2e62025b7e0e64958e1c
Andrea McBride <31637885+amcbride001@users.noreply.github.com>
2018-09-08 15:38:40 -0400

    added changes

---
+++ src/main/java/virtualpet/VirtualPet.java
@@ -0,0 +1,18 @@
+package virtualpet;
+
+public class VirtualPet {
+
+	public static void main(String[] args) {
+		// Create a tick() method that represents the passage of time.
+	}
+	
+ public static void feed() { 
+	int feedpet = 27;
+ 
+	
+
+	
+		
+	}
+
+}
